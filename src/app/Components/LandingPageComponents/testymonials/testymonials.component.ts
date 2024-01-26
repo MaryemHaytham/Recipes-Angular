8a02659c145018deb06caeaa4a634c36a0dd5f3b
MaryemHaytham <maryemhaythamm@gmail.com>
2024-01-27 00:25:55 +0300

    first commit

---
+++ src/app/Components/LandingPageComponents/testymonials/testymonials.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-testymonials',
+  templateUrl: './testymonials.component.html',
+  styleUrls: ['./testymonials.component.css']
+})
+export class TestymonialsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/LandingPageComponents/home/home.component';
+import { HowWeWorkComponent } from './Components/LandingPageComponents/how-we-work/how-we-work.component';
+import { AboutUsComponent } from './Components/LandingPageComponents/about-us/about-us.component';
+import { RecipesComponent } from './Components/LandingPageComponents/recipes/recipes.component';
+import { TestymonialsComponent } from './Components/LandingPageComponents/testymonials/testymonials.component';
+import { ContactUsComponent } from './Components/LandingPageComponents/contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HowWeWorkComponent,
+    AboutUsComponent,
+    RecipesComponent,
+    TestymonialsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
